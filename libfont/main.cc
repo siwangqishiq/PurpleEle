1357ab9a41f29fca67b8059e058b0782a55df6a7
yixin.panyi <panyi@yixin.im>
2023-07-18 18:12:11 +0800

    update

--- libfont/main.cc
+++ libfont/main.cc
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <string>
 #include "../libjson/json.hpp"
+#include <algorithm>
 
 #include "ft2build.h"
 #include FT_FREETYPE_H  
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+
+struct CharData{
+    int fontWidth;
+    int fontHeight;
+    int outWidth;
+    int outHeight;
+    int offsetX;
+    int offsetY;
+    int bearingY;
+    int *pData;
+};
 
 const float FONT_DEFAULT_SIZE = 64.0f;
 
@@ -17,6 +29,20 @@ void blit(unsigned char *src , uint32_t *dst , int x , int y , int width , int h
         for(int j = 0 ; j < width ;j++){
             if(src[i * width + j] == 0){
                 dst[(y + i) * strideWidth + x + j] = 0x00000000;
+            }else{
+                //abgr
+                dst[(y + i) * strideWidth + x + j] = 0xffffffff;
+            }
+        }//end for j
+    }
+}
+
+void blit2(int *src , uint32_t *dst , int x , int y , int width , int height ,
+            int strideWidth){
+    for(int i = 0 ; i < height ;i++){
+        for(int j = 0 ; j < width ;j++){
+            if(src[i * width + j] == 0){
+                dst[(y + i) * strideWidth + x + j] = 0xffff0000;
             }else{
                 //abgr
                 dst[(y + i) * strideWidth + x + j] = 0xffffffff;
@@ -88,6 +114,7 @@ int exportFonts(){
             continue;
         }
 
+
         // std::wcout << "left " << left << "  top " << top << std::endl;
 
         if (FT_Load_Char(face, ch, FT_LOAD_RENDER)){
@@ -119,9 +146,10 @@ int exportFonts(){
         charInfoJson->putString("value" , value);
         charInfoJson->putInt("width" , fontWidth);
         charInfoJson->putInt("height" , fontHeight);
-        charInfoJson->putInt("bearingX" , glyph->bitmap_left);
-        charInfoJson->putInt("bearingY" , glyph->bitmap_top);
+        charInfoJson->putInt("offset_x" , glyph->bitmap_left);
+        charInfoJson->putInt("offset_y" , glyph->bitmap_top);
         charInfoJson->putString("texture" , ToWideString(currentTexFileName));
+
         //纹理坐标计算
         float texLeft = (float)left / (float)outTexWidth;
         float texTop = (float)(top + (fontSize - fontHeight)) / (float)outTexHeight;
@@ -159,6 +187,7 @@ int exportFonts(){
         }else{
             left += fontSize;
         }
+
     }//end for each
 
     stbi_write_png(currentTexFileName.c_str() , outTexWidth, outTexHeight, 4 , textureDst , 0);
@@ -172,6 +201,66 @@ int exportFonts(){
     auto charList = result->getJsonArray("list");
 
     std::wcout << "total size " << charListArray->size() << std::endl;
+    return 0;
+}
+
+int buildWcharTexture(wchar_t wChar , FT_Face &face , int fontHeight , CharData &charData){
+    if (FT_Load_Char(face, wChar, FT_LOAD_RENDER)){
+        std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+        return -1;
+    }
+
+    FT_Bitmap fontBit = face->glyph->bitmap;
+
+    charData.fontWidth = fontBit.pitch;
+    charData.fontHeight = fontBit.rows;
+
+    charData.outWidth = std::max(static_cast<int>(face->glyph->advance.x / 64) , 
+        charData.fontWidth);
+    charData.outHeight = std::max(fontHeight , charData.fontHeight);
+    charData.bearingY = face->glyph->bitmap_top;
+
+    charData.pData = new int[charData.outWidth * charData.outHeight];
+    
+    //init;
+    for(int i = charData.outHeight - 1 ; i >= 0; i--){
+        for(int j = 0; j < charData.outWidth;j++){
+            charData.pData[charData.outWidth * i + j] = 0;
+        }//end for j
+    }//end for i
+
+
+    int offset_x = (charData.outWidth - fontBit.pitch) / 2.0;
+    if(offset_x < 0){
+        offset_x = 0;
+    }
+    charData.offsetX = offset_x;
+
+    int offset_y = (charData.outHeight - face->glyph->bitmap_top);
+    if(offset_y + face->glyph->bitmap.rows > charData.outHeight){
+        offset_y = charData.outHeight - face->glyph->bitmap.rows;
+    }
+    if(offset_y < 0){
+        offset_y = 0;
+    }
+    charData.offsetY = offset_y;
+
+
+    std::cout << "outWidth   :" << charData.outWidth << std::endl;
+    std::cout << "outHeight  :" << charData.outHeight << std::endl;
+    std::cout << "fontWidth  :" << charData.fontWidth << std::endl;
+    std::cout << "fontHeight :" << charData.fontHeight << std::endl;
+    std::cout << "offsetx    :" << charData.offsetX << std::endl;
+    std::cout << "offsety    :" << charData.offsetY << std::endl;
+    std::cout << "bearingY   :" << charData.bearingY << std::endl;
+
+    for(int i = 0 ;  i < fontBit.rows ; i++ ){
+        for(int j = 0 ; j < fontBit.pitch ; j++){
+            if(fontBit.buffer[i * fontBit.pitch + j] != 0){
+                charData.pData[(i + offset_y) * charData.outWidth + j + offset_x] = 1;
+            }
+        }
+    }
 
     return 0;
 }
@@ -232,7 +321,7 @@ int printChar(wchar_t wChar){
     if(offset_x < 0){
         offset_x = 0;
     }
-    
+
     int offset_y = outputHeight - face->glyph->bitmap_top;
     if(offset_y + face->glyph->bitmap.rows > outputHeight){
         offset_y = outputHeight - face->glyph->bitmap.rows;
@@ -250,7 +339,6 @@ int printChar(wchar_t wChar){
         }
     }
 
-
     //print on console
     for(int i = 0 ; i < outputHeight;i++){
         for(int j = 0; j < outputWidth;j++){
@@ -265,13 +353,118 @@ int printChar(wchar_t wChar){
 }
 
 
+int exportFonts2(){
+    std::string fontName = "yahei";
+    
+    FT_Library ftLib;
+
+    if (FT_Init_FreeType(&ftLib)){
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        return -1;
+    }
+
+    FT_Face face;
+    std::string fontPath = fontName + ".ttc";
+    if (FT_New_Face(ftLib, fontPath.c_str(), 0, &face)){
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        return -1;
+    }
+
+    const int fontSize = 32;
+
+    FT_Set_Pixel_Sizes(face , 0, fontSize);
+
+    auto outputJson = JsonObject::create();
+    auto charListArray = JsonArray::create();
+    outputJson->putJsonArray("list" , charListArray);
+
+    std::wstring content = ReadTextFileAsWstring("test.txt");
+    // std::wstring content = L"abc";
+    std::cout << "char file size : " << content.length() << std::endl;
+
+    const int outTexWidth = 1024;
+    const int outTexHeight = 1024;
+
+    uint32_t *textureDst = new uint32_t[outTexWidth * outTexHeight];
+
+    int left = 0;
+    int top = 0;
+    int progress = 0;
+    int currentFileIndex = 0;
+    std::string currentTexFileName = "font_texture_"+std::to_string(currentFileIndex)+".png";
+
+    for(wchar_t ch : content){
+        progress++;
+        std::cout << progress << " / " << content.length() << std::endl;
+        if(ch == L' ' || ch == L'\t' || ch == L'\n' || ch == L'\r'){
+            continue;
+        }
+
+        if (FT_Load_Char(face, ch, FT_LOAD_RENDER)){
+            std::wcout<< ch << " ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+
+        CharData charData;
+        if(buildWcharTexture(ch , face , fontSize , charData) < 0){
+            std::wcout<< ch << " ERROR::FREETYTPE: Failed to load char data" << std::endl;
+            continue;
+        }
+
+        //print on console
+        for(int i = 0 ; i < charData.outHeight;i++){
+            for(int j = 0; j < charData.outWidth;j++){
+                std::cout << charData.pData[i * charData.outWidth + j];
+            }
+            std::cout<< std::endl;
+        }
+
+        std::cout << "left " << left << " top " << top << std::endl; 
+
+        if(left + charData.outWidth >= outTexWidth){
+            left = 0.0;
+            top += charData.outHeight;
+        }
+
+        blit2(charData.pData , textureDst , left , top, 
+            charData.outWidth , charData.outHeight , outTexWidth);
+        
+        if(left + charData.outWidth >= outTexWidth){
+            left = 0.0;
+            top += charData.outHeight;
+        }else{
+            left += charData.outWidth;
+        }
+
+        delete charData.pData;
+
+
+    }//end for each
+
+    FT_Done_Face(face);
+
+    stbi_write_png(currentTexFileName.c_str() , outTexWidth, outTexHeight, 4 , textureDst , 0);
+    delete []textureDst;
+
+    WriteStringToFile("char_config.json", outputJson->toJsonString());
+
+    JsonParser parser;
+    auto str = outputJson->toJsonString();
+    auto result = parser.parseJsonObject(str);
+    auto charList = result->getJsonArray("list");
+
+    std::wcout << "total size " << charListArray->size() << std::endl;
+    return 0;
+}
+
 int main(){
     // exportFonts();
-
-    std::wstring str = L"j@";
-    for(auto &ch : str){
-        printChar(ch);
-    }//end for 
+    // std::wstring str = L"j";
+    // for(auto &ch : str){
+    //     printChar(ch);
+    // }//end for 
     
+    exportFonts2();
+
     return 0;
 }
